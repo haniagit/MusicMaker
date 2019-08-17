87769f0c7567b7a10613bef9db1c59516b785573
Hania <haniagit08@gmail.com>
2019-08-17 19:29:17 +0200

    tries

--- src/com/hania/GUIv1.java
+++ src/com/hania/GUIv1.java
@@ -1,16 +1,35 @@
 package com.hania;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GUIv1 {
+public class GUIv1 implements ActionListener {
+    private JButton button;
 
     public static void main(String[] args) {
+        GUIv1 guIv1 = new GUIv1();
+        guIv1.go();
+    }
+
+    public void go(){
         JFrame frame = new JFrame();
-        JButton button = new JButton("Click me");
+        button = new JButton("Click me");
+
+
+
+        button.addActionListener(this);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().add(button);
         frame.setSize(300, 300);
         frame.setVisible(true);
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+     button.setText("Clicked!");
+    }
+
 }
