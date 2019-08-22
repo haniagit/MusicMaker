1c7f6fea635dbd0a8b618c760f538a8b8a15eaf4
Hania <haniagit08@gmail.com>
2019-08-22 22:26:28 +0200

    animation update

--- src/com/hania/Animation.java
+++ src/com/hania/Animation.java
@@ -38,6 +38,9 @@ public class Animation {
 
     class MyImagePanel extends JPanel {
         public void paintComponent(Graphics g){
+            g.setColor(Color.white);
+            g.fillRect(0,0,this.getWidth(),this.getHeight());
+
             g.setColor(Color.green);
             g.fillOval(x,y,40,40);
         }
